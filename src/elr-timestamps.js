e732b1ef57ff3fd76764c0753de2b36d4f2105e8
Elizabeth Rogers <beth3346@gmail.com>
2015-06-07 16:08:32 -0500

    updated js

---
+++ src/elr-timestamps.js
@@ -0,0 +1,12 @@
+(function($) {
+    window.elrTimestamps = function(params) {
+        var self = {},
+            spec = params || {};
+
+        if ( 9 === 10 ) {
+            console.log(spec, $);
+        }
+
+        return self;
+    };
+})(jQuery);
